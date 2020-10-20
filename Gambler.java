6584011c1ba86dcbeaa2a1b20e2a1a67bc734e1a
asmitamthorat <asmita26296@gmail.com>
2020-10-20 15:05:52 +0530

    added win or loose amount

--- Gambler.java
+++ Gambler.java
@@ -1,8 +1,26 @@
 package com.workshop;
 
+class GamblerGame {
+	int everyDayAmount=100;
+	int betAmount=1;
+	int winAmount=1;
+	int loseAmount=1;
+	public void game() {
+		
+		
+	}
+}
+
+
+
+
+
+
 public class Gambler {
 	public static void main(String asgs[]) {
-		System.out.println("welcome to java workShop ");
+		System.out.println("welcome to gambler game");
+		GamblerGame gamGame=new GamblerGame();
+		
 	}
 
 }
