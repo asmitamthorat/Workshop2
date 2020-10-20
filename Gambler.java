67c1f92dc2ae1d59668cc859ec5c2f7e393e9487
asmitamthorat <asmita26296@gmail.com>
2020-10-20 17:06:10 +0530

    added functionality to get total amount for 20 days

--- Gambler.java
+++ Gambler.java
@@ -2,35 +2,46 @@ package com.workshop;
 
 class GamblerGame {
 	final static int BEATAMOUNT = 1;
-	final static int STACKAMOUNT=100;
+	final static int STACKAMOUNT = 100;
 
-	public void StartGame() {
-		
-		int beatAmount=STACKAMOUNT;
-		while (beatAmount > 50 && beatAmount < 150) {
+	public int startGame() {
+		int betAmount = STACKAMOUNT;
+
+		while (betAmount > 50 && betAmount < 150) {
 			int option = (int) ((Math.random() * 2) + 1);
 			if (option == 1) {
-				System.out.println("winning");
-				beatAmount = beatAmount + BEATAMOUNT;
-
+				betAmount = betAmount + BEATAMOUNT;
 			}
 			if (option == 2) {
-				System.out.println("looseing");
-				beatAmount = beatAmount - BEATAMOUNT;
-
+				betAmount = betAmount - BEATAMOUNT;
 			}
 		}
-		System.out.println("Money after gamble" + beatAmount);
-
+		return betAmount;
 	}
+
+	public void gameAmount() {
+		int totalDays = 20;
+		int totalAmount = 0;
+		for (int i = 1; i <= totalDays; i++) {
+			int amount = startGame();
+			System.out.println("Total amount made for " + i + " is " + amount);
+			if (amount == 50) {
+				totalAmount = totalAmount - 50;
+			}
+			if (amount == 150) {
+				totalAmount = totalAmount + 150;
+			}
+		}
+		System.out.println("total amount: " + totalAmount);
+	}
+
 }
 
 public class Gambler {
 	public static void main(String asgs[]) {
 		System.out.println("welcome to gambler game");
 		GamblerGame gameGame = new GamblerGame();
-		gameGame.StartGame();
-
+		gameGame.gameAmount();
 	}
 
 }
