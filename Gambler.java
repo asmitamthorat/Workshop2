0a668c5f48f6f48d2df8db07dd673e660559f89a
asmitamthorat <asmita26296@gmail.com>
2020-10-20 16:22:03 +0530

    Repeated StartGame till Stackamount rise or loose by 50%

--- Gambler.java
+++ Gambler.java
@@ -1,32 +1,36 @@
 package com.workshop;
 
 class GamblerGame {
-	int Amount=100;
-	int betAmount=1;
-    final static int winAmount=1;
-    final static int loseAmount=1;
-    
-	public void Startgame() {
-	int	option=(int)((Math.random()*2)+1);
-		if(option==1) {
-			System.out.println("winning");
-			Amount=Amount+winAmount;
-		}
-		if(option==2) {
-			System.out.println("looseing");
-			Amount=Amount-loseAmount;
-		}
+	final static int BEATAMOUNT = 1;
+	final static int STACKAMOUNT=100;
+
+	public void StartGame() {
 		
+		int beatAmount=STACKAMOUNT;
+		while (beatAmount > 50 && beatAmount < 150) {
+			int option = (int) ((Math.random() * 2) + 1);
+			if (option == 1) {
+				System.out.println("winning");
+				beatAmount = beatAmount + BEATAMOUNT;
+
+			}
+			if (option == 2) {
+				System.out.println("looseing");
+				beatAmount = beatAmount - BEATAMOUNT;
+
+			}
+		}
+		System.out.println("Money after gamble" + beatAmount);
+
 	}
 }
-
 
 public class Gambler {
 	public static void main(String asgs[]) {
 		System.out.println("welcome to gambler game");
-		GamblerGame gameGame=new GamblerGame();
-		gameGame.Startgame();
-		
+		GamblerGame gameGame = new GamblerGame();
+		gameGame.StartGame();
+
 	}
 
 }
