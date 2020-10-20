e0bdeb1f0c216e13db6cfa06152dcfee1e92cc33
asmitamthorat <asmita26296@gmail.com>
2020-10-20 15:29:14 +0530

    provinding option to check for win or loose

--- Gambler.java
+++ Gambler.java
@@ -1,25 +1,31 @@
 package com.workshop;
 
 class GamblerGame {
-	int everyDayAmount=100;
+	int Amount=100;
 	int betAmount=1;
-	int winAmount=1;
-	int loseAmount=1;
-	public void game() {
-		
+    final static int winAmount=1;
+    final static int loseAmount=1;
+    
+	public void Startgame() {
+	int	option=(int)((Math.random()*2)+1);
+		if(option==1) {
+			System.out.println("winning");
+			Amount=Amount+winAmount;
+		}
+		if(option==2) {
+			System.out.println("looseing");
+			Amount=Amount-loseAmount;
+		}
 		
 	}
 }
 
 
-
-
-
-
 public class Gambler {
 	public static void main(String asgs[]) {
 		System.out.println("welcome to gambler game");
-		GamblerGame gamGame=new GamblerGame();
+		GamblerGame gameGame=new GamblerGame();
+		gameGame.Startgame();
 		
 	}
 
